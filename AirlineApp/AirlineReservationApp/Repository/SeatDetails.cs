ebdfbee1378dccbecfd2a2d36b077060b207719e
MogilireddigariDeepthi <98934515+MogilireddigariDeepthi@users.noreply.github.com>
2022-03-16 22:56:09 +0530

    App created and database connected

---
+++ AirlineApp/AirlineReservationApp/Repository/SeatDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace AirlineReservationApp.Repository
+{
+    public partial class SeatDetails
+    {
+        public SeatDetails()
+        {
+            BookingDetails = new HashSet<BookingDetails>();
+        }
+
+        public int SeatNo { get; set; }
+        public string SeatType { get; set; }
+
+        public virtual ICollection<BookingDetails> BookingDetails { get; set; }
+    }
+}
