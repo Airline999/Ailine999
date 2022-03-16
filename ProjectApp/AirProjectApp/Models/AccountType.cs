4978ca1356b0d12ccafd054e9c272417b3687612
MogilireddigariDeepthi <98934515+MogilireddigariDeepthi@users.noreply.github.com>
2022-03-16 21:33:46 +0530

    config changed

---
+++ ProjectApp/AirProjectApp/Models/AccountType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace AirProjectApp.Models
+{
+    public partial class AccountType
+    {
+        public AccountType()
+        {
+            UserDetails = new HashSet<UserDetails>();
+        }
+
+        public int AccountId { get; set; }
+        public string AccountType1 { get; set; }
+
+        public virtual ICollection<UserDetails> UserDetails { get; set; }
+    }
+}
---
+++ ProjectApp/AirProjectApp/Models/FlightDetails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace AirProjectApp.Models
+{
+    public partial class FlightDetails
+    {
+        public FlightDetails()
+        {
+            BookingDetails = new HashSet<BookingDetails>();
+        }
+
+        public int FlightId { get; set; }
+        public string FlightName { get; set; }
+        public string FromLocation { get; set; }
+        public string ToLocation { get; set; }
+        public TimeSpan? ArrivalTime { get; set; }
+        public TimeSpan? DepartureTime { get; set; }
+        public int? NoOfSeats { get; set; }
+        public int CreatedBy { get; set; }
+
+        public virtual UserDetails CreatedByNavigation { get; set; }
+        public virtual ICollection<BookingDetails> BookingDetails { get; set; }
+    }
+}
